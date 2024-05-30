096a2b87ab01c8019ba5040b8f65b83b254e565f
LucasCatuyama <lucas.catuyama@outlook.com>
2024-05-30 17:14:38 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mercadinho`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
