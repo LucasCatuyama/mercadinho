8fd5e273fd5fdc5dd9d31856afab1e83e6c94a7b
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-06-01 21:25:04 -0300

    adjusted img width

--- src/components/CardWithImage.jsx
+++ src/components/CardWithImage.jsx
@@ -4,7 +4,7 @@ import breadImage from '../images/bread.jpg';
 const CardWithImage = () => {
   return (
     <div className="flex flex-col items-start gap-3 w-[283px] h-auto p-4 bg-card-bg-beige border-2 rounded-2xl border-dark-green text-text-green">
-      <img src={breadImage} alt="" />
+      <img src={breadImage} alt="" className="w-full"/>
       <h1 className="text-xl/normal font-bold leading-7">Product name</h1>
       <p>Price</p>
       <p>
